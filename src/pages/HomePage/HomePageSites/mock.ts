3abac1bc1d00c680e54d3447305aafc7dd674adb
Artem Karakulov <karakulow@yandex.ru>
2021-10-19 19:24:51 +0400

    fix: edit links on otherSites

--- src/pages/HomePage/HomePageSites/mock.ts
+++ src/pages/HomePage/HomePageSites/mock.ts
@@ -1,6 +1,5 @@
 import { SiteI } from '../../../types';
 import image1 from '../../../assets/img/farming/icon-site1.png';
-import image2 from '../../../assets/img/farming/icon-site2.png';
 import image3 from '../../../assets/img/farming/icon-site3.png';
 import image4 from '../../../assets/img/farming/icon-site4.png';
 import image5 from '../../../assets/img/farming/icon-site5.png';
@@ -10,11 +9,6 @@ export const otherSites: SiteI[] = [
         image: image1,
         title: 'NFT Marketplace',
         link: 'https://nftmarketplace.rocknblock.io/'
-    },
-    {
-        image: image2,
-        title: 'Farming',
-        link: 'https://farming.rocknblock.io'
     },
     {
         image: image3,
