09120d71f282cf2eebb64e0e4e5de4883eda581c
Artem Karakulov <karakulow@yandex.ru>
2021-10-21 21:51:23 +0400

    fix: slide size

--- src/pages/HomePage/HomePageSites/HomePageSites.tsx
+++ src/pages/HomePage/HomePageSites/HomePageSites.tsx
@@ -1,55 +1,34 @@
 import {
-    Fragment, FunctionComponent, h
+    FunctionComponent, h
 } from 'preact';
 import {
     Swiper, SwiperSlide
 } from 'swiper/react';
 import 'swiper/swiper.min.css';
-import {
-    ContentWrapper, SiteCard, Title
-} from '../../../components';
-import {
-    CONTENT_TAGS, TEXT_ALIGNS, TITLE_TAGS
-} from '../../../constants';
+import { SiteCard } from '../../../components';
 import styles from './HomePageSites.module.scss';
 import { otherSites } from './mock';
 
 const HomePageSites: FunctionComponent = () => (
-    <Fragment>
-        <ContentWrapper
-            tag={CONTENT_TAGS.SECTION}
-            classNameWrapper={styles.sectionSites}
+    <div className={styles.sliderWrapper}>
+        <Swiper
+            slidesPerView='auto'
+            centeredSlides
+            grabCursor
+            className={styles.slider}
+            loop
         >
-            <Title
-                tag={TITLE_TAGS.H2}
-                theme={TITLE_TAGS.H3}
-                align={TEXT_ALIGNS.CENTER}
-                className={styles.sectionSites__title}
-                isUppercase
-            >
-                HEad
-            </Title>
-        </ContentWrapper>
-        <div className={styles.sliderWrapper}>
-            <Swiper
-                slidesPerView='auto'
-                centeredSlides
-                grabCursor
-                className={styles.slider}
-                loop
-            >
-                {otherSites.map((site) => (
-                    <SwiperSlide className={styles.slide}>
-                        <SiteCard
-                            image={site.image}
-                            title={site.title}
-                            link={site.link}
-                        />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
-    </Fragment>
+            {otherSites.map((site) => (
+                <SwiperSlide className={styles.slide}>
+                    <SiteCard
+                        image={site.image}
+                        title={site.title}
+                        link={site.link}
+                    />
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    </div>
 );
 
 export default HomePageSites;
